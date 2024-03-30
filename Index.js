0fe9cc3db9576916a28fe1b3d427bd3933fa836f
ZinoTrust <zinotrust@gmail.com>
2024-03-30 14:19:01 +0100

    initial commit

---
+++ Index.js
@@ -0,0 +1,436 @@
+export const nigeriaStates = [
+  {
+    name: "Abia",
+    doorstepDelivery: 5000,
+    pickupService: {
+        delivery: 8000,
+        pickupStation: [
+            { name: "Aba Pickup Center" },
+            { name: "Umuahia Pickup Center" },
+        ]
+    },
+    payOnDelivery: 15000,
+  },
+  {
+    name: "Adamawa",
+    doorstepDelivery: 5000,
+    pickupService: {
+        delivery: 10000,
+        pickupStation: [
+            { name: "Yola Pickup Center" }
+        ]
+    },
+    payOnDelivery: 15000,
+  },
+  {
+    name: "Akwa Ibom",
+    doorstepDelivery: 7000,
+    pickupService: {
+        delivery: 8000,
+        pickupStation: [
+            { name: "Uyo Pickup Center" }
+        ]
+    },
+    payOnDelivery: 15000,
+  },
+  {
+    name: "Anambra",
+    doorstepDelivery: 5000,
+    pickupService: {
+        delivery: 8000,
+        pickupStation: [
+            { name: "Awka Pickup Center" },
+            { name: "Onitsha Pickup Center" },
+        ]
+    },
+    payOnDelivery: 15000,
+  },
+  {
+    name: "Bauchi",
+    doorstepDelivery: 5000,
+    pickupService: {
+        delivery: 10000,
+        pickupStation: [
+            { name: "Bauchi Pickup Center" }
+        ]
+    },
+    payOnDelivery: 15000,
+  },
+  {
+    name: "Bayelsa",
+    doorstepDelivery: 5000,
+    pickupService: {
+        delivery: 10000,
+        pickupStation: [
+            { name: "Yenagoa Pickup Center" }
+        ]
+    },
+    payOnDelivery: 15000,
+  },
+  {
+    name: "Benue",
+    doorstepDelivery: 5000,
+    pickupService: {
+        delivery: 10000,
+        pickupStation: [
+            { name: "Makurdi Pickup Center" }
+        ]
+    },
+    payOnDelivery: 15000,
+  },
+  {
+    name: "Borno",
+    doorstepDelivery: 5000,
+    pickupService: {
+        delivery: 10000,
+        pickupStation: [
+            { name: "Maiduguri Pickup Center" }
+        ]
+    },
+    payOnDelivery: 15000,
+  },
+  {
+    name: "Cross River",
+    doorstepDelivery: 5000,
+    pickupService: {
+        delivery: 10000,
+        pickupStation: [
+            { name: "Calabar Pickup Center" }
+        ]
+    },
+    payOnDelivery: 15000,
+  },
+  {
+    name: "Delta",
+    doorstepDelivery: 6000,
+    pickupService: {
+        delivery: 10000,
+        pickupStation: [
+            { name: "Asaba Pickup Center" },
+            { name: "Warri Pickup Center" },
+        ]
+    },
+    payOnDelivery: 15000,
+  },
+  {
+    name: "Ebonyi",
+    doorstepDelivery: 5000,
+    pickupService: {
+        delivery: 10000,
+        pickupStation: [
+            { name: "Abakaliki Pickup Center" }
+        ]
+    },
+    payOnDelivery: 15000,
+  },
+  {
+    name: "Edo",
+    doorstepDelivery: 3000,
+    pickupService: {
+        delivery: 10000,
+        pickupStation: [
+            { name: "Benin Pickup Center" }
+        ]
+    },
+    payOnDelivery: 15000,
+  },
+  {
+    name: "Ekiti",
+    doorstepDelivery: 3000,
+    pickupService: {
+        delivery: 8000,
+        pickupStation: [
+            { name: "Ado-Ekiti Pickup Center" }
+        ]
+    },
+    payOnDelivery: 15000,
+  },
+  {
+    name: "Enugu",
+    doorstepDelivery: 5000,
+    pickupService: {
+        delivery: 10000,
+        pickupStation: [
+            { name: "Enugu Pickup Center" }
+        ]
+    },
+    payOnDelivery: 15000,
+  },
+  {
+    name: "Gombe",
+    doorstepDelivery: 5000,
+    pickupService: {
+        delivery: 10000,
+        pickupStation: [
+            { name: "Gombe Pickup Center" }
+        ]
+    },
+    payOnDelivery: 15000,
+  },
+  {
+    name: "Imo",
+    doorstepDelivery: 5000,
+    pickupService: {
+        delivery: 8000,
+        pickupStation: [
+            { name: "Owerri Pickup Center" }
+        ]
+    },
+    payOnDelivery: 15000,
+  },
+  {
+    name: "Jigawa",
+    doorstepDelivery: 5000,
+    pickupService: {
+        delivery: 10000,
+        pickupStation: [
+            { name: "Dutse Pickup Center" }
+        ]
+    },
+    payOnDelivery: 15000,
+  },
+  {
+    name: "Kaduna",
+    doorstepDelivery: 5000,
+    pickupService: {
+        delivery: 10000,
+        pickupStation: [
+            { name: "Mando Pickup Center" }
+        ]
+    },
+    payOnDelivery: 15000,
+  },
+  {
+    name: "Kano",
+    doorstepDelivery: 5000,
+    pickupService: {
+        delivery: 10000,
+        pickupStation: [
+            { name: "Ajingi Pickup Center" }
+        ]
+    },
+    payOnDelivery: 15000,
+  },
+  {
+    name: "Katsina",
+    doorstepDelivery: 5000,
+    pickupService: {
+        delivery: 10000,
+        pickupStation: [
+            { name: "Katsina Pickup Center" }
+        ]
+    },
+    payOnDelivery: 15000,
+  },
+  {
+    name: "Kebbi",
+    doorstepDelivery: 5000,
+    pickupService: {
+        delivery: 10000,
+        pickupStation: [
+            { name: "Birnin Kebbi Pickup Center" }
+        ]
+    },
+    payOnDelivery: 15000,
+  },
+  {
+    name: "Kogi",
+    doorstepDelivery: 5000,
+    pickupService: {
+        delivery:  10000,
+        pickupStation: [
+            { name: "Lokoja Pickup Center" }
+        ]
+    },
+    payOnDelivery: 15000,
+  },
+  {
+    name: "Kwara",
+    doorstepDelivery: 3000,
+    pickupService: {
+        delivery: 10000,
+        pickupStation: [
+            { name: "Ilorin Pickup Center" }
+        ]
+    },
+    payOnDelivery: 15000,
+  },
+  {
+    name: "Lagos",
+    region: [
+      { name: "Agege", delivery: 3500 },
+      { name: "Ajeromi-Ifelodun", delivery: 4000 },
+      { name: "Alimosho", delivery: 4000 },
+      { name: "Amuwo-Odofin", delivery: 5000 },
+      { name: "Apapa", delivery: 5000 },
+      { name: "Badagry", delivery: 6000 },
+      { name: "Epe", delivery: 7000 },
+      { name: "Eti-Osa", delivery: 5000 },
+      { name: "Ibeju-Lekki", delivery: 7000 },
+      { name: "Ifako-Ijaye", delivery: 4000 },
+      { name: "Ikeja", delivery: 3000 },
+      { name: "Ikorodu", delivery: 4000 },
+      { name: "Kosofe", delivery: 4000 },
+      { name: "Lagos Island", delivery: 4000 },
+      { name: "Lagos Mainland", delivery: 4000 },
+      { name: "Mushin", delivery: 4000 },
+      { name: "Ojo", delivery: 5000 },
+      { name: "Oshodi-Isolo", delivery: 4000 },
+      { name: "Shomolu", delivery: 4000 },
+      { name: "Surulere", delivery: 4000 },
+    ],
+    payOnDelivery: [
+      { name: "Agege", delivery: 3500 },
+      { name: "Ajeromi-Ifelodun", delivery: 6000 },
+      { name: "Alimosho", delivery: 6000 },
+      { name: "Amuwo-Odofin", delivery: 7500 },
+      { name: "Apapa", delivery: 7500 },
+      { name: "Badagry", delivery: 10000 },
+      { name: "Epe", delivery: 10000 },
+      { name: "Eti-Osa", delivery: 7500 },
+      { name: "Ibeju-Lekki", delivery: 10000 },
+      { name: "IIfako-Ijaye", delivery: 6000 },
+      { name: "Ikeja", delivery: 5000 },
+      { name: "Ikorodu", delivery: 6000 },
+      { name: "Kosofe", delivery: 6000 },
+      { name: "Lagos Island", delivery: 6000 },
+      { name: "Lagos Mainland", delivery: 6000 },
+      { name: "Mushin", delivery: 6000 },
+      { name: "Ojo", delivery: 7500 },
+      { name: "Oshodi-Isolo", delivery: 6000 },
+      { name: "Shomolu", delivery: 6000 },
+      { name: "Surulere", delivery: 6000 },
+    ]
+  },
+  {
+    name: "Nasarawa",
+    doorstepDelivery: 5000,
+    pickupService: {
+        delivery: 10000,
+        pickupStation: [
+            { name: "Akwanga Pickup Center" }
+        ]
+    },
+    payOnDelivery: 15000,
+  },
+  {
+    name: "Niger",
+    doorstepDelivery: 5000,
+    pickupService: {
+        delivery: 10000,
+        pickupStation: [
+            { name: "Muya Pickup Center" }
+        ]
+    },
+    payOnDelivery: 15000,
+  },
+  {
+    name: "Ogun",
+    doorstepDelivery: 3000,
+    pickupService: {
+        delivery: 6000,
+        pickupStation: [
+            { name: "Abeokuta Pickup Center" }
+        ]
+    },
+    payOnDelivery: 10000,
+  },
+  {
+    name: "Ondo",
+    doorstepDelivery: 3000,
+    pickupService: {
+        delivery: 10000,
+        pickupStation: [
+            { name: "Akure Pickup Center" }
+        ]
+    },
+    payOnDelivery: 15000,
+  },
+  {
+    name: "Osun",
+    doorstepDelivery: 3000,
+    pickupService: {
+        delivery: 10000,
+        pickupStation: [
+            { name: "Osogbo Pickup Center" }
+        ]
+    },
+    payOnDelivery: 15000,
+  },
+  {
+    name: "Oyo",
+    doorstepDelivery: 3000,
+    pickupService: {
+        delivery: 60000,
+        pickupStation: [
+            { name: "Ibadan Pickup Center" },
+            { name: "Ogbomoso Pickup Center" },
+        ]
+    },
+    payOnDelivery: 10000,
+  },
+  {
+    name: "Plateau",
+    doorstepDelivery: 5000,
+    pickupService: {
+        delivery: 10000,
+        pickupStation: [
+            { name: "Jos Pickup Center" }
+        ]
+    },
+    payOnDelivery: 15000,
+  },
+  {
+    name: "Rivers",
+    doorstepDelivery: 6000,
+    pickupService: {
+        delivery: 10000,
+        pickupStation: [
+            { name: "Garrison Pickup Center" }
+        ]
+    },
+    payOnDelivery: 15000,
+  },
+  {
+    name: "Sokoto",
+    doorstepDelivery: 5000,
+    pickupService: {
+        delivery: 10000,
+        pickupStation: [
+            { name: "Sokoto Pickup Center" }
+        ]
+    },
+    payOnDelivery: 15000,
+  },
+  {
+    name: "Taraba",
+    doorstepDelivery: 5000,
+    pickupService: {
+        delivery: 10000,
+        pickupStation: [
+            { name: "Jalingo Pickup Center" }
+        ]
+    },
+    payOnDelivery: 15000,
+  },
+  {
+    name: "Yobe",
+    doorstepDelivery: 5000,
+    pickupService: {
+        delivery: 10000,
+        pickupStation: [
+            { name: "Damaturu Pickup Center" }
+        ]
+    },
+    payOnDelivery: 15000,
+  },
+  {
+    name: "Zamfara",
+    doorstepDelivery: 5000,
+    pickupService: {
+        delivery: 10000,
+        pickupStation: [
+            { name: "Gusau Pickup Center" }
+        ]
+    },
+    payOnDelivery: 15000,
